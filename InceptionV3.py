6414302441d8a0b706db487a841048eeea4b9d4e
Khaled Skairek <khaled.skairek@gmail.com>
2019-11-27 21:43:29 +0100

    For review

--- InceptionV3.py
+++ InceptionV3.py
@@ -8,7 +8,7 @@ from random import shuffle
 import sklearn
 
 
-def generator(samples, batch_size=36):
+def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -51,9 +51,9 @@ with open('my_data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
-print("before", lines[0])
-lines.pop(0)
-print("after", lines[0])
+print("First line of csv file", lines[0])
+# lines.pop(0)
+# print("after", lines[0])
 
 from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
@@ -62,48 +62,54 @@ train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 # joblib.dump(training_data, "training_data.p")
 
 # Set our batch size
-batch_size=36
-
+batch_size=32
 # compile and train the model using the generator function
 train_generator = generator(train_samples, batch_size=batch_size)
 validation_generator = generator(validation_samples, batch_size=batch_size)
 
 
-from keras.layers import Input, Lambda, Cropping2D, Dense, GlobalAveragePooling2D
+from keras.layers import Input, Lambda, Cropping2D, Dense, GlobalAveragePooling2D, Flatten
 from keras.applications.inception_v3 import InceptionV3
 from keras.models import Model
 import tensorflow as tf
 
 
-input_width = 320
-input_height = 140 - 60
+size_cropped = 90
 # Using Inception with ImageNet pre-trained weights
 inception = InceptionV3(weights='imagenet', include_top=False,
-                        input_shape=(input_height, input_width, 3))
+                        input_shape=(size_cropped, 320, 3))
 # Freeze what already trained
 for layer in inception.layers:
     layer.trainable = False
 
 # Input layer
-image_input = Input(shape=(160, 320, 3))
+# image_input = Input(shape=(160, 320, 3))
+image_input = Input(shape=(size_cropped, 320, 3))
 
 # Cropping layer
-cropped_input = Cropping2D(cropping=((60,20), (0,0)), input_shape=(160,320,3))(image_input)
+cropped_input = Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3))(image_input)
 
 # Lambda layer (normalization)
-resized_input = Lambda(lambda image: image / 255.0 - 0.5, input_shape=(80,320,3))(cropped_input)
+resized_input = Lambda(lambda pixel: pixel / 255.0 - 0.5, input_shape=(size_cropped,320,3))(cropped_input)
 
 # Inception V3 layers
-inp = inception(resized_input)
+# inp = inception(resized_input)
+inp = inception(image_input)
 
 # Global average pooling layer
 glob_avg_pool = GlobalAveragePooling2D()(inp)
+# glob_avg_pool = inp
 
 # Fully connected layer
-pre_output = Dense(512, activation='relu')(glob_avg_pool)
+# Activation should be None, otherwise no negative steering values will be allowed
+Dense_layer1 = Dense(100, activation=None )(glob_avg_pool)
+
+Dense_layer2 = Dense(50, activation=None )(Dense_layer1)
+
+Dense_layer3 = Dense(10, activation=None )(Dense_layer2)
 
 # Output layer (fully connected layer)
-predictions = Dense(1, activation='softmax')(pre_output)
+predictions = Dense(1, activation=None)(Dense_layer3)
 
 # model = Model(inputs=inception_input, outputs=predictions)
 model = Model(inputs=image_input, outputs=predictions)
@@ -112,6 +118,8 @@ model = Model(inputs=image_input, outputs=predictions)
 model.compile(optimizer='adam', loss='mse')
 
 print(model.summary())
+# print(inception.summary())
+
 
 from keras.callbacks.callbacks import ModelCheckpoint, EarlyStopping
 # checkpoint = ModelCheckpoint(filepath='model.h5', monitor='val_loss', save_best_only=True)
@@ -121,11 +129,11 @@ model.fit_generator(train_generator,
  steps_per_epoch=ceil(len(train_samples)/batch_size),
  validation_data=validation_generator,
  validation_steps=ceil(len(validation_samples)/batch_size),
- # callbacks=[checkpoint, stopper],
- epochs=1,
+#  callbacks=[checkpoint, stopper],
+ epochs=2,
  verbose=1)
 
-model.save('model.h5')
+# model.save('model.h5')
 
 
 
